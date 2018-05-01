8cb4076f3b8888f03ff642c84a7861ab089d8333
Adam Braden <adambr@microsoft.com>
2018-05-01 12:52:01 -0700

    updated headers

--- Microsoft.Toolkit.Win32.UI.Controls/WinForms/GraphLogin/GraphLoginComponent.cs
+++ Microsoft.Toolkit.Win32.UI.Controls/WinForms/GraphLogin/GraphLoginComponent.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿// ******************************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -94,9 +106,16 @@ namespace Microsoft.Toolkit.Win32.UI.Controls.WinForms
                 return false;
             }
 
+            if (Scopes == null)
+            {
+                // error
+                return false;
+            }
+
             // Initialize the MicrosoftGraphService
             if (!MicrosoftGraphService.Instance.Initialize(clientId, delegatedPermissionScopes: Scopes))
             {
+                // error
                 return false;
             }
 
@@ -107,7 +126,7 @@ namespace Microsoft.Toolkit.Win32.UI.Controls.WinForms
             }
             catch (Exception ex)
             {
-                Debug.WriteLine(ex.Message);
+                // error
                 return false;
             }
 
