3d215a75ef9aa3a90395ee88886681b1de77d87a
Richard Murillo <rimuri@microsoft.com>
2018-08-13 14:41:25 -0700

    Update LongRunningJavaScriptTest
    Now correctly fires events

--- Microsoft.Toolkit.Win32/Tests/UnitTests.WebView.WinForms/FunctionalTests/LongRunningJavaScript/LongRunningJavaScriptTests.cs
+++ Microsoft.Toolkit.Win32/Tests/UnitTests.WebView.WinForms/FunctionalTests/LongRunningJavaScript/LongRunningJavaScriptTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Microsoft.Toolkit.Win32.UI.Controls.Test.WebView.Shared;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Should;
@@ -11,45 +12,67 @@ namespace Microsoft.Toolkit.Win32.UI.Controls.Test.WinForms.WebView.FunctionalTe
     [TestClass]
     public class LongRunningJavaScriptTests : HostFormWebViewContextSpecification
     {
-        private string _result;
         private bool _slowEventRaised;
         private string _content = @"
-<h1>TEST</h1>
+<!doctype html>
+<head>
 <script>
-function mySlowFunction(baseNumber) {
-  console.time('mySlowFunction');
-  var result = 0,
-      i;
+    function loadScript(delay) {
+        const startTime = new Date().getTime();
+        let currentTime = new Date().getTime();
 
-  for(i = Math.pow(baseNumber, 10); i >= 0; i--) {
-    result += Math.atan(i) * Math.tan(i);
-  }
+        // Create a long running script by busy waiting until 20s have elapsed
+        do {
+            // Note: Must allocate in the inner loop to afford Chakra an opportunity to
+            // detect the long running script.
+            currentTime = new Date().getTime();
+        } while ((currentTime - startTime) < delay);
 
-  console.timeEnd('mySlowFunction');
-  return result;
-};
+        window.external.notify('Script running too long');
+    }
 </script>
+</head>
+<body><h1>Long running script test page</h1></body>
+</html>
 ";
 
         protected override void Given()
         {
             base.Given();
             WebView.IsJavaScriptEnabled = true;
+            WebView.IsScriptNotifyAllowed = true;
 
             // BUG: The content causes browsers to show prompt when loaded externally without a problem
             WebView.LongRunningScriptDetected += (o, e) =>
             {
                 WriteLine($"LongRunningScriptDetected: {e.ExecutionTime}");
                 _slowEventRaised = true;
-                e.StopPageScriptExecution = true;
+            };
 
+            WebView.ScriptNotify += (o, e) =>
+            {
+                // Got to the end, didn't raise LongRunningScriptDetected
                 Form.Close();
             };
 
-            WebView.NavigationCompleted += (o, e) =>
+            WebView.NavigationCompleted += async (o, e) =>
                 {
-                    _result = WebView.InvokeScriptAsync("mySlowFunction", "3000000").Result;
-                    Form.Close();
+                    try
+                    {
+                        await WebView.InvokeScriptAsync("loadScript", "2000");
+                    }
+                    catch (Exception ex)
+                    {
+                        const uint E_ABORT = unchecked(0x80004004);
+
+                        switch ((uint)ex.HResult)
+                        {
+                            case E_ABORT:
+                                break;
+                            default:
+                                throw;
+                        }
+                    }
                 };
         }
 
@@ -60,11 +83,10 @@ function mySlowFunction(baseNumber) {
 
         [TestMethod]
         [Timeout(TestConstants.Timeouts.Longest)]
-        [Ignore("LongRunningScriptDetected event is not raised")]
+        //[Ignore("LongRunningScriptDetected event is not raised")]
         public void LongRunningJavaScriptEventRaised()
         {
             _slowEventRaised.ShouldBeTrue();
-            _result.ShouldNotBeNull();
         }
     }
 }
